bd63121bb406c1288f03e9a12fa1308ae2563e12
Wassapon Watanakeesuntorn <wassapon.w@gmail.com>
2019-01-08 16:56:20 +0900

    Add variables type

--- src/ccm_interface.h
+++ src/ccm_interface.h
@@ -5,23 +5,23 @@ class ccm_interface
 {
     ccm_interface();
 
-//     block
-//     lib = c(1, NROW(block))
-//     pred = lib
-//     norm = 2
-//     E = 1 
-//     tau = 1
-//     tp = 0
-//     num_neighbors = "e+1", 
-//     lib_sizes = seq(10, 100, by = 10)
-//     bool random_libs = TRUE
-//     num_samples = 100
-//     bool replace = TRUE
-//     lib_column = 1
-//     target_column = 2
-//     bool first_column_time = FALSE
-//     RNGseed = NULL
-//     exclusion_radius = NULL
-//     bool epsilon = NULL
-//     bool silent = FALSE
+    // list block
+    // double lib = c(1, NROW(block));
+    // double pred = lib;
+    // double norm = 2;
+    // int E = 1;
+    // double tau = 1
+    // double tp = 0;
+    // char num_neighbors = "e+1";
+    // int lib_sizes = seq(10, 100, by = 10);
+    // bool random_libs = TRUE;
+    // double num_samples = 100;
+    // bool replace = TRUE;
+    // double lib_column = 1;
+    // int target_column = 2;
+    // bool first_column_time = FALSE;
+    // bool RNGseed = NULL;
+    // double exclusion_radius = NULL;
+    // bool epsilon = NULL;
+    // bool silent = FALSE;
 };
